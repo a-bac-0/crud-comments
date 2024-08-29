9c3b8bacf5ed90297f6305930dc216dfa851b6ba
a-bac-0 <anca.bacria@thevallians.com>
2024-08-29 05:10:14 +0200

    final commit

--- src/services.js
+++ src/services.js
@@ -9,14 +9,43 @@ const API_URL = "http://localhost:3000/comments"
 
 // * los invertimos para formar CRUD ---NO ES NECESARIO--
 // 3º CREATE - POST
-async function createComment(params) {
-    
+
+// 
+
+async function updateComment() {
+    const form = document.getElementById('addNewComment')
+    const product_data = document.getElementByName('product').value;
+    const newComment = {
+        "name": form.name.value,
+        "product": product_data,
+        "description": form.description.value
+    }
+
+    if (form.name.value||product_data||form.description.value) {
+        return alert("All fields requirednpm r")
+    }
+    // const commentInput = document.getElementById("newComment");
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newComment)
+    })
+
+    if (response.ok) {
+        printComments()
+    }
 }
+    // const data = (await response).json() //transforma los datos a json
+    // if (response.ok) {
+    //     renderTask
+        
+    // }
+    // return data
 
 
 // 1º READ - GET
 async function getAllComments() {
-    const response = await fetch(API_URL, {
+    const response = await fetch (API_URL, {
         method: 'GET',
         headers: {"Content-Type": "application/json"},
     })
@@ -26,26 +55,50 @@ async function getAllComments() {
 
 const listTag = document.getElementById('commentsList')
 
-async function printComment() {
-    const comments = await getAllComments ()
-    comments.map ((comment) => {
+// 1.0 PRINT ---SE REUTILIZA MÁS ADELANTE
+async function printComments(){
+    const comments = await getAllComments()
+    listTag.innerHTML = '';
+    comments.map((comment) => {
         listTag.innerHTML += `<li>
-            <p>${comment.id}</p>
+            
             <p>${comment.name}</p>
             <p>${comment.product}</p>
             <p>${comment.description}</p>
-            <button onclick = "deleteComment(${comment.id})">Delete</button>
+            <button onclick = "updateComment(${comment.id}, { name: "Your Alias", product: "Your Purchase", description: "Tell us more"})">Update</button>
+            <button onclick = "deleteComment('${comment.id}')">Delete</button>
         </li>`
     })
 }
 
-printComment()
+printComments()
 
 // 4º UPDATE - PUT
-async function updateComment(params) {
-    
-}
+async function updateComment(commentId) {
+    const form = document.getElementById('updateCommentForm');
+    const product_data = document.getElementsByName('product')[0].value;
+    const updatedComment = {
+        "name": form.name.value,
+        "product": product_data,
+        "description": form.description.value
+    };
 
+    if (!form.name.value || !product_data || !form.description.value) {
+        return alert("All fields are required");
+    }
+    // const commentInput = document.getElementById("newComment");
+    const response = await fetch(`${API_URL}/${commentId}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newComment)
+    })
+
+    if (response.ok) {
+        printComments();
+    } else {
+        console.error('Error al actualizar el comentario:', response.statusText);
+    }
+}
 
 // 2º DELETE - DELETE
 async function deleteComment(id) {
@@ -55,6 +108,11 @@ async function deleteComment(id) {
     })
 
     const deletedComment = await response.json()
+
+    if(response.ok) {
+        printComments() //para refrescar después de eliminar
+    }
+
     return deletedComment
 }
 
